c56b914555190ea09ec29ddb2c667a533a885e3e
Alphabeater <19984045+Alphabeater@users.noreply.github.com>
2020-11-30 20:09:04 -0300

    Stage 5/6: On a coffee loop

--- Main.kt
+++ Main.kt
@@ -1,36 +1,31 @@
 package machine
-import java.util.Scanner
-
-val s = Scanner(System.`in`)
+import java.lang.Exception
 
 open class Coffee(var water: Int, var milk: Int, var beans: Int, var cups: Int, var money: Int) {
 
-    fun printState() = println("The coffee machine has:\n" +
-            "$water of water\n" +
-            "$milk of milk\n" +
-            "$beans of coffee beans\n" +
-            "$cups of disposable cups\n" +
-            "$money of money\n")
     fun buy(option: Int) {
         when (option) {
             1 -> makeCoffee(waterNeeded = 250, beansNeeded = 16, cost = 4) //espresso
             2 -> makeCoffee(350, 75, 20, 7) //latte
             3 -> makeCoffee(200, 100, 12, 6) //cappuccino
-            else -> println("No such option.")
+            else -> throw Exception("Invalid option.")
         }
     }
 
     private fun makeCoffee(waterNeeded: Int, milkNeeded: Int = 0, beansNeeded: Int, cost: Int) {
         if (cups > 0) {
-            if (waterNeeded > water || milkNeeded > milk || beansNeeded > beans) println("Not enough resouces")
+            if (waterNeeded > water) throw Exception("Sorry, not enough water!")
+            else if (milkNeeded > milk) throw Exception("Sorry, not enough milk!")
+            else if (beansNeeded > beans) throw Exception("Sorry, not enough coffee beans!")
             else {
+                println("I have enough resources, making you a coffee!")
                 water -= waterNeeded
                 milk -= milkNeeded
                 beans -= beansNeeded
                 money += cost
                 cups -= 1
             }
-        } else println("Not enough cups.")
+        } else throw Exception("Sorry, not enough cups!")
     }
 
     fun fill(addWater: Int, addMilk: Int, addBeans: Int, addCups: Int) {
@@ -44,34 +39,58 @@ open class Coffee(var water: Int, var milk: Int, var beans: Int, var cups: Int, 
         println("I gave you $$money")
         money = 0
     }
+
+    fun remaining() = println("\nThe coffee machine has:\n" +
+        "$water of water\n" +
+        "$milk of milk\n" +
+        "$beans of coffee beans\n" +
+        "$cups of disposable cups\n" +
+        "$$money of money")
 }
 
 fun main() {
     val coffeeMachine = Coffee(400, 540, 120, 9, 550)
 
-    coffeeMachine.printState()
-    print("Write action (buy, fill, take): ")
-    when (readLine()!!) {
-        "buy" -> {
-            print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
-            coffeeMachine.buy(readLine()!!.toInt())
+    loop@do {
+        print("\nWrite action (buy, fill, take, remaining, exit): ")
+        try {
+            when (readLine()!!) {
+                "buy" -> {
+                    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
+                    val option = readLine()!!
+                    if (option == "back") continue@loop
+                    coffeeMachine.buy(option.toInt())
+                    continue@loop
+                }
+                "fill" -> {
+                    print("Write how many ml of water do you want to add: ")
+                    val water = readLine()!!.toInt()
+                    print("Write how many ml of milk do you want to add: ")
+                    val milk = readLine()!!.toInt()
+                    print("Write how many grams of coffee beans do you want to add: ")
+                    val beans = readLine()!!.toInt()
+                    print("Write how many disposable cups of coffee do you want to add: ")
+                    val cups = readLine()!!.toInt()
+                    coffeeMachine.fill(water, milk, beans, cups)
+                    continue@loop
+                }
+                "take" -> {
+                    coffeeMachine.take()
+                    continue@loop
+                }
+                "remaining" -> {
+                    coffeeMachine.remaining()
+                    continue@loop
+                }
+                "exit" -> break@loop
+                else -> {
+                    throw Exception("Invalid option.")
+                }
+            }
+        } catch (e: Exception) {
+            println(e.message)
+            continue@loop
         }
-        "fill" -> {
-            print("Write how many ml of water do you want to add: ")
-            val water = readLine()!!.toInt()
-            print("Write how many ml of milk do you want to add: ")
-            val milk = readLine()!!.toInt()
-            print("Write how many grams of coffee beans do you want to add: ")
-            val beans = readLine()!!.toInt()
-            print("Write how many disposable cups of coffee do you want to add: ")
-            val cups = readLine()!!.toInt()
-            coffeeMachine.fill(water, milk, beans, cups)
-        }
-        "take" -> {
-            coffeeMachine.take()
-        }
-        else -> println("No such option.")
-    }
-    println()
-    coffeeMachine.printState()
+    } while (true)
+
 }
